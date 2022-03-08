11baa6d72ef0c684672ab2127a42ddccf6622520
9anjan9 <100926163+9anjan9@users.noreply.github.com>
2022-03-08 13:16:33 +0530

    Update webService.tf

--- webService.tf
+++ webService.tf
@@ -1,6 +1,6 @@
 resource "azurerm_app_service" "serviceAppName" {
   name                = var.webAppService
   location            = var.location
-  resource_group_name = azurerm_resource_group.RGname.name
+  resource_group_name = azurerm_resource_group.myRG.name
   app_service_plan_id = azurerm_app_service_plan.myPlan.id
 }
