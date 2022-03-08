b75d161546fb6f6a624c1e5230ce39a4840ec784
9anjan9 <100926163+9anjan9@users.noreply.github.com>
2022-03-08 13:25:01 +0530

    Update service-plan-app.tf

--- service-plan-app.tf
+++ service-plan-app.tf
@@ -1,7 +1,7 @@
 resource "azurerm_app_service_plan" "myPlan" {
   name                = var.servicePlan
   location            = var.location
-  resource_group_name = azurerm_resource_group.myRG.name
+  resource_group_name = azurerm_resource_group.resGrName.name
 
   sku {
     tier = "Standard"
